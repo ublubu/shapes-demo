f0777f728dd424aa791628fd78b7ca2017263d32
Kynan Rilee <kynan.rilee@gmail.com>
2015-09-12 14:21:43 -0700

    pull demo code into its own package

--- default.nix
+++ default.nix
@@ -1,2 +1,17 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./tile-rider.nix { }
+{ mkDerivation, base, containers, either, lens, linear
+, monad-extras, mtl, sdl2, shapes, stdenv, vector, zippers
+}:
+mkDerivation {
+  pname = "shapes-demo";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base containers either lens linear monad-extras mtl sdl2 shapes
+    vector zippers
+  ];
+  homepage = "https://github.com/ublubu/shapes-demo";
+  description = "demos for the 'shapes' package";
+  license = stdenv.lib.licenses.unfree;
+}
